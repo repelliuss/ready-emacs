0de8641f31cf365623e42a95af36159c8209ed69
repelliuss <repelliuss@gmail.com>
2021-07-07 23:43:19 +0300

    Add window keymap

--- rps/base/submodules/window.el
+++ rps/base/submodules/window.el
@@ -1,17 +1,23 @@
 ;;; window.el -*- lexical-binding: t; -*-
 
-(use-package emacs
-  :after (meow)
-  :bind (:map meow-leader-keymap
-         ("w h" . windmove-left)
-         ("w j" . windmove-down)
-         ("w k" . windmove-up)
-         ("w l" . windmove-right)
-         ("w H" . windmove-swap-states-left)
-         ("w J" . windmove-swap-states-down)
-         ("w K" . windmove-swap-states-up)
-         ("w L" . windmove-swap-states-right)
-         ("w d" . delete-window)
-         ("w D" . delete-other-windows)
-         ("w s" . split-window-below)
-         ("w v" . split-window-right)))
+(bind-keys
+ :map (defvar rps-window-map (make-sparse-keymap))
+ ("w h" . windmove-left)
+ ("w j" . windmove-down)
+ ("w k" . windmove-up)
+ ("w l" . windmove-right)
+ ("w H" . windmove-swap-states-left)
+ ("w J" . windmove-swap-states-down)
+ ("w K" . windmove-swap-states-up)
+ ("w L" . windmove-swap-states-right)
+ ("w d" . delete-window)
+ ("w D" . delete-other-windows)
+ ("w s" . split-window-below)
+ ("w v" . split-window-right))
+
+(after! which-key
+  (add-to-list 'which-key-replacement-alist '(("w$" . "prefix") . (nil . "Window")))
+  ;; TODO: remove wind, window, windows and wrapping -
+  )
+
+(provide 'rps-window '(editor))
