c3874cb24055657fd0d42f8ec60a9f7d8ad58b33
repelliuss <repelliuss@gmail.com>
2021-07-08 10:10:15 +0300

    golden-ratio: Remove resizing which-key buffer

--- rps/editor/packages/golden-ratio.el
+++ rps/editor/packages/golden-ratio.el
@@ -3,4 +3,9 @@
 (use-package golden-ratio
   :demand
   :config
+  (after! which-key
+    (add-to-list 'golden-ratio-inhibit-functions
+                 (lambda ()
+                   (and which-key--buffer
+                        (window-live-p (get-buffer-window which-key--buffer))))))
   (golden-ratio-mode 1))
