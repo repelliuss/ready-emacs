98afdb3abadb27a7d92d82b3bc215df862e67dac
repelliuss <repelliuss@gmail.com>
2021-07-08 10:55:43 +0300

    Add init time for debug

--- init.el
+++ init.el
@@ -29,3 +29,9 @@
   :config
   (setq gcmh-idle-delay 5)
   (gcmh-mode 1))
+
+;; TODO: Remove this
+(message "*** Emacs loaded in %s with %d garbage collections."
+     (format "%.2f seconds"
+             (float-time
+              (time-subtract after-init-time before-init-time))) gcs-done)
