64aeee24272011fae806b761745bc7062140dcc0
repelliuss <repelliuss@gmail.com>
2021-09-13 20:26:40 +0300

    Update

--- ready/modules/base/submodules/optimize.el
+++ ready/modules/base/submodules/optimize.el
@@ -12,7 +12,7 @@
 
 ;; Reduce rendering/line scan work for Emacs by not rendering cursors or regions
 ;; in non-focused windows.
-(setq-default cursor-in-non-selected-windows t)
+(setq-default cursor-in-non-selected-windows nil)
 (setq highlight-nonselected-windows nil)
 
 ;; More performant rapid scrolling over unfontified regions. May cause brief
