3fa6ac21d106d86d2b0a63c95be909d88962490a
Sami Batuhan Basmaz Ölmez <repelliuss@gmail.com>
2022-05-09 00:52:34 +0300

    add bind repeat

--- local/enable/init/submodules/50-bind.el
+++ local/enable/init/submodules/50-bind.el
@@ -1,8 +1,10 @@
 ;;; bind.el -*- lexical-binding: t; -*-
 
 ;; TODO: add support for unbind
-;; TODO: add support for repeat
 ;; TODO: support multiple-maps through map argument being list
+
+(defcustom bind-repeat-generate-name #'bind--repeat-prepend-repeat
+  "Default.")
 
 (defmacro bind--normalize-bindings (arg)
   `(if (consp (car ,arg))
@@ -40,14 +42,21 @@
       map-s-or-fn
     `(list ,@map-s-or-fn)))
 
+(defmacro bind--main-map (bind-first)
+  `(cond
+    ((symbolp ,bind-first) ,bind-first)
+    ((fboundp (car ,bind-first)) (cadr ,bind-first))
+    (t (caar ,bind-first))))
+
 (defmacro bind (&rest rest)
   (let ((first (car rest))
 	(second (cadr rest)))
     (if (or (stringp second)
 	    (vectorp second)
 	    (fboundp (car second)))
-	`(bind--done (bind--normalize-first ,first)
-		     (list ,@(cdr rest)))
+	`(let* ((metadata '(:main-map ,(bind--main-map first))))
+	   (bind--done (bind--normalize-first ,first)
+		       (list ,@(cdr rest))))
       `(bind--many ,@rest))))
 
 (defun bind-prefix (prefix &rest bindings)
@@ -63,7 +72,7 @@
       (setq bindings (cddr bindings)))
     new-bindings))
 
-(defun bind-command (file &rest bindings)
+(defun bind-autoload (file &rest bindings)
   (declare (indent 1))
   (bind--normalize-bindings bindings)
   (let ((it-bindings bindings))
@@ -72,3 +81,26 @@
 	(autoload def file nil t))
       (setq it-bindings (cddr it-bindings))))
   bindings)
+
+(defun bind--repeat-prepend-repeat (main-map-name)
+  (concat "repeat-" main-map-name))
+
+(defun bind--repeat (main-map &rest bindings)
+  (bind--normalize-bindings bindings)
+  (let ((repeat-map
+	 (intern (funcall bind-repeat-generate-name
+			  (symbol-name main-map)))))
+    (if (not (boundp repeat-map))
+	(set repeat-map (make-sparse-keymap)))
+    (set-keymap-parent (symbol-value repeat-map) (symbol-value main-map))
+    (let ((it-bindings bindings))
+      (while it-bindings
+	(let ((def (cadr it-bindings)))
+	  (put def 'repeat-map repeat-map))
+	(setq it-bindings (cddr it-bindings)))))
+  bindings)
+
+(defmacro bind-repeat (&rest bindings)
+  (declare (indent 1))
+  `(bind--repeat (plist-get metadata :main-map)
+		 ,@bindings))
